26654e7c964d407ae6f34ebe7cd57f2c311cedac
史少龙 <shishaolong@wesai.com>
2018-06-23 19:11:27 +0800

    初始化 测试

--- app/src/main/java/com/ssl/common/MainActivity.kt
+++ app/src/main/java/com/ssl/common/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ssl.common
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ssl.common.library.utils.SSLDeviceUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rv_text.setText("名称："+SSLDeviceUtil.getVersion(this))
     }
 }
